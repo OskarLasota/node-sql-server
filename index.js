f1a8dc918f39b293aed1808e786f723cb09bccd1
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-13 21:12:59 +0100

    handling register

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const bodyParser = require('body-parser')
 const app = express();
 require('dotenv').config();
 
 const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  )
+  app.use(bodyParser.json())
 
-//this should be hidden
 const connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
     database: process.env.database,
 });
+
 
 connection.connect(err => {
     if(err){
@@ -23,9 +31,7 @@ connection.connect(err => {
         console.log("connection success");
     }
 });
-console.log(connection);
 
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("to go /products to see products")
@@ -37,15 +43,37 @@ app.get('/products/add', (req,res) =>{
     connection.query(INSERT_PRODUCTS_QUERY, (err, results) => {
         if(err){
             return res.send(err)
-        }else{
+        }else{ 
             return res.send('successfully added product');
         }
     });
 });
 
 
+
+app.post('/register', (req, res) =>{
+    console.log("post request /register");
+    const {
+        first_name,
+        last_name,
+        email,
+        password
+    } = req.body;
+    
+    const REGISTER_USER_QUERY = `INSERT INTO users(first_name, last_name, email, password) VALUES ("${first_name}", "${last_name}", "${email}", "${password}")`;
+    connection.query(REGISTER_USER_QUERY, (err, results) => {
+        if(err){
+            return res.send(err)
+        }else{ 
+            return res.send('successfully registered!');
+        }
+    });
+})
+
+
 app.get('/products', (req,res) => {
     connection.query(SELECT_ALL_PRODUCTS_QUERY, (err,results) => {
+        console.log("get request /products");
         if(err){
             return res.send(err);
         }else{
